5aaee4dd7c51b50449f25c2cb21dca3e47aa78cc
IvanRuskevych <ivan.ruskevych@gmail.com>
2023-04-14 14:02:23 +0000

    Deploying to gh-pages from @ IvanRuskevych/code-wizards-team-project-js@d89de9dd2d07e8711c5d717133b2db4a7291bc48 🚀

--- index.38905bca.js
+++
@@ -1,2 +0,0 @@
-console.log("Hellow Word");let e=document.querySelector(".mask");window.addEventListener("load",(()=>{e.classList.add("hide"),setTimeout((()=>{e.remove()}),600)}));
-//# sourceMappingURL=index.38905bca.js.map
