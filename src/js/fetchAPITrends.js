d30ce71a372d8bff597c78dc40376370678282e3
Matviy <antipin.m9971@gmail.com>
2023-04-14 10:18:30 +0300

    add getGenres function

--- src/js/fetchAPITrends.js
+++ src/js/fetchAPITrends.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 const API_KEY = '7c0c458e245909c66f3397c50f32766a'
 // axios.defaults.baseURL = `https://api.themoviedb.org/3/movie/550?api_key=7c0c458e245909c66f3397c50f32766a`;
-
-export const getData = async (page) => {
+let pageState = 1;
+export const getAPITrend = async (page) => {
 	const { data } = await axios.get('https://api.themoviedb.org/3/trending/all/day?api_key=7c0c458e245909c66f3397c50f32766a', {
 		params: {
 			page
@@ -10,18 +10,28 @@ export const getData = async (page) => {
 	})
 	return await data;
 }
-// export const getData = async (page) => {
-// 	const { data } = await axios.get('https://api.themoviedb.org/3/trending/all/day?api_key=7c0c458e245909c66f3397c50f32766a&page=2')
-// 	return await data;
-// }
-getData(3).then(data=> console.log(data))
+
+async function getAPIGenres(page) {
+	const {data} = await axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=7c0c458e245909c66f3397c50f32766a&')
+	return await data.genres;
+}
+
+// getAPIGenres().then(result => {
+// 	const { genres } = result;
+// 	return genres;
+// })
+
+getAPITrend(pageState).then(data=> console.log(data))
 function resetGallery(parent) {
 	parent.innerHTML = '';
 }
 
-async function  getGenres(data) {
-	const genres = await fetch('')
+async function getGenres(genre) {
+	// const dbFilms = await getAPITrend(pageState);
+	// const { results } = dbFilms;
+	const genresArray = await getAPIGenres();
+	return genresArray.find(item => genre == item.id).name;
 }
-
+getGenres(28).then(results=> console.log(results))
 resetGallery(document.querySelector('.gallery__list'))
 
