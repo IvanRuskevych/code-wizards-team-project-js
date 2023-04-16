ed0d2e40799ea66f3dfd3d65af89eb8af224bd36
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-04-16 17:03:33 +0200

    ok

--- src/js/searchAPI.js
+++ src/js/searchAPI.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import {renderList} from "./fetchAPITrends.js"
+import Notiflix from 'notiflix';
 // const API_KEY = 'f9f6d3f14431911aa9602e018f8e4b77' матвея ключ 7c0c458e245909c66f3397c50f32766a
 let pageState = 1;
 export const instance = axios.create({
@@ -12,31 +13,57 @@ async function getMoviesByName(name) {
 	return await instance.get(`&query=${ name }`);
 }
 
-//   const formElement = document.forms.header__form;
 
 const searchBarForm = document.querySelector('#header__form');
-// console.log(searchBarForm);
-// я матвея другое название - подставить
-// const allMoviesPlace = document.querySelector('.gallery__list');
-// console.log(allMoviesPlace);
+// let headerFormInput = document.querySelector(".header__input")
+// Доступ input для вывода сообщения используем headerFormInput.parentElement в функции 
+// let headerFormInput = searchBarForm.elements.query
+
+
+// headerFormInput.addEventListener("focus", onHeaderFormInput)
+// console.log(headerFormInput).value
+
+
 
 searchBarForm.addEventListener('submit', onSearchBarFormSubmit);
 
 async function onSearchBarFormSubmit(event) {
 	event.preventDefault();
 
-
-
-	//   const onSearchBarInput = formElement.query.value;
-
+	//   const onInput = formElement.query.value;
+// Значение поля input
 	const onSearchBarInput = event.target.elements.query.value;
 
+// Если поле не заполнено - запрос не отправляем на сервер при нажатии
+	if(!onSearchBarInput){
+		Notiflix.Notify.failure('Please enter a keyword to continue the search');
+		// тут можно дополнительно вызвывать функцию sendMessage () с каким то сообщением"поле поиска не заполнено"
+		// sendMessage ()
+		event.preventDefault();
+		return
+	}
 
-	console.log(onSearchBarInput);
+	
+
+	// console.log(onSearchBarInput);
 	// Обьект получаем
 
 	getMoviesByName(onSearchBarInput).then(({ data }) => {
-		console.log(data.results)
+
+		// записываем результат данных с сервера в переменную
+		const arrayOfResults = data.results;
+		console.log(arrayOfResults)
+// Если результат не найден по ключевым словам выдаём сообщение ошибку
+		if (!arrayOfResults.length > 0) {
+			Notiflix.Notify.failure('Search result not successful. Enter the correct movie name.');
+			// Меняем класс  <p> и делаем видемым
+			// elementP.style.display = 'block';
+			searchBarForm.reset();
+			return
+		}
+
+
+		renderList(document.querySelector('.gallery__list'), data.results)
 	// 	const gallaryItem = data.results.map(element => {
 	// 		// console.log(getGenres(element.genre_ids[0]))
 	// 		return `
@@ -55,7 +82,42 @@ async function onSearchBarFormSubmit(event) {
 	// 	}).join('');
 	// 	document.querySelector('.gallery__list').innerHTML = gallaryItem
 		
-		renderList(document.querySelector('.gallery__list'), data.results)
-		
+	// После нажатия submit скидываем форму
+	
+	searchBarForm.reset();
     })
 }
+
+
+// function onHeaderFormInput (){
+// 	console.log(headerFormInput).value
+
+
+// }
+
+
+
+// onHeaderFormInput()
+
+
+
+// Получаем значение поля импута placeholder для работы с placeholder , При фокусе placeholder убирается при снятии фокуса появляется
+
+// Значение placeholder
+// headerFormInputPlaceholder ="Movie search";
+// let headerFormInputPlaceholder = searchBarForm.query.placeholder
+// console.log(headerFormInputPlaceholder);
+// searchBarForm.addEventListener("focus", function(){
+// 	headerFormInputPlaceholder = "";
+// })
+// console.log(headerFormInputPlaceholder);
+// searchBarForm.addEventListener("blur", function(){
+// 	searchBarForm.query.placeholder = headerFormInputPlaceholder;
+// })
+
+// Функция добавления сообщения если не через
+function sendMessage () {
+
+	
+	headerFormInput.parentElement.insertAdjacentHTML ("beforeend", `<p class="header-form__error">Форма не отправлена</p>`)
+  }
