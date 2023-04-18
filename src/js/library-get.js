62cd3d100a5c062437b658baf3e8f996709a4698
unknown <ivan.ruskevych@gmail.com>
2023-04-18 03:12:00 +0300

    status W-Q + logika

--- src/js/library-get.js
+++ src/js/library-get.js
@@ -33,27 +33,17 @@ function onBtnLibraryClick(e) {
 
 function renderLibrary(parent, collection, status) {
   if (status === 'library') {
-    const gallaryItem = collection
-      .map(element => {
-        return `
-		<li class="gallery__item" data-id="${element.id}">
-        <img src="${element.poster}" alt="" class="gallery__item-img">
-         <p class="gallery__item-descr">
-            <span class="gallery__item-name">
-             ${element.title}
-           </span>
-            <span class="gallery__item-genres">
-             ${element.genres} | ${element.release}
-          </span>
-        </p>
-      </li>
-		`;
-      })
-      .join('');
-    return (parent.innerHTML = gallaryItem);
+    return (parent.innerHTML = markupMoviesFromLocalStorage(
+      collection,
+      'watched'
+    ));
   }
-  const gallaryItem = collection
-    .filter(movie => movie.status === status)
+  return (parent.innerHTML = markupMoviesFromLocalStorage(collection, status));
+}
+
+function markupMoviesFromLocalStorage(collection, statusForMarkup) {
+  return collection
+    .filter(movie => movie.status === statusForMarkup)
     .map(element => {
       return `
 		<li class="gallery__item" data-id="${element.id}">
@@ -70,7 +60,6 @@ function renderLibrary(parent, collection, status) {
 		`;
     })
     .join('');
-  return (parent.innerHTML = gallaryItem);
 }
 
 // ЗМІНА СТИЛІВ ДЛЯ WATCHED/QUEUE
@@ -79,14 +68,13 @@ function changeStyleOnButtonsLibrary(e) {
   if (e.target.dataset.status_library === 'watched') {
     if (e.target.classList.contains('library-current-btn')) {
       return;
-    } else {
-      btnWatchedRef.classList.add('library-current-btn');
-      btnQueueRef.classList.remove('library-current-btn');
-      return;
     }
+    elementClassRemove(btnQueueRef, 'library-current-btn');
+    elementClassAdd(btnWatchedRef, 'library-current-btn');
+    return;
   }
-  btnWatchedRef.classList.remove('library-current-btn');
-  btnQueueRef.classList.add('library-current-btn');
+  elementClassRemove(btnWatchedRef, 'library-current-btn');
+  elementClassAdd(btnQueueRef, 'library-current-btn');
   return;
 }
 
@@ -96,26 +84,32 @@ btnQueueRef.addEventListener('click', changeStyleOnButtonsLibrary);
 // ЗМІНА СТИЛІВ ДЛЯ HOME / LIBRARY
 
 const btnLibraryBox = document.querySelector('.library-btn-box');
-console.log(btnLibraryBox);
 const btnHomeRef = document.querySelector('[data-status_library="home"]');
-console.log(btnHomeRef);
 
 btnMyLibraryRef.addEventListener('click', changeStyleOnBtnHomeLibrary);
 btnHomeRef.addEventListener('click', changeStyleOnBtnHomeLibrary);
-const paginationRef = document.querySelector('#pagination');
 
 function changeStyleOnBtnHomeLibrary(e) {
   if (e.target.dataset.status_library === 'library') {
     if (e.target.classList.contains('btn--current')) {
-      return;
-    } else {
-      btnMyLibraryRef.classList.add('btn--current');
-      btnHomeRef.classList.remove('btn--current');
-      formSearchRef.classList.add('visually-hidden');
-      btnLibraryBox.classList.add('library-btn-visual');
+      elementClassRemove(btnQueueRef, 'library-current-btn');
+      elementClassAdd(btnWatchedRef, 'library-current-btn');
       return;
     }
+    elementClassAdd(btnMyLibraryRef, 'btn--current');
+    elementClassRemove(btnHomeRef, 'btn--current');
+    elementClassAdd(formSearchRef, 'visually-hidden');
+    elementClassAdd(btnLibraryBox, 'library-btn-visual');
+    return;
   }
-  btnMyLibraryRef.classList.remove('btn--current');
-  btnHomeRef.classList.add('btn--current');
+  elementClassRemove(btnMyLibraryRef, 'btn--current');
+  elementClassAdd(btnHomeRef, 'btn--current');
+}
+
+function elementClassAdd(elemRef, cls) {
+  elemRef.classList.add(cls);
+}
+
+function elementClassRemove(elemRef, cls) {
+  elemRef.classList.remove(cls);
 }
