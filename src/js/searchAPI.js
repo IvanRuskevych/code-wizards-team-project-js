b0aabe2a120450bc2c811e8f63a512d541c12052
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-04-19 15:29:03 +0200

    serchBar

--- src/js/searchAPI.js
+++ src/js/searchAPI.js
@@ -19,126 +19,86 @@ export const instance = axios.create({
     'https://api.themoviedb.org/3/search/movie?api_key=f9f6d3f14431911aa9602e018f8e4b77&language=en-US&page=1&include_adult=false',
 });
 
-async function getMoviesByName(name, page) {
+async function getMoviesByName (name, page) {
   return await instance.get(`&query=${name}&page=${page}`);
 }
 
-
+async function getMovieByNameRelease (name, page, releaseDate) {
+  return await instance.get(
+    `&query=${name}&page=${page}&release_date=${releaseDate}`
+  );
+}
 
 // Получаем доступ к форме
 const searchBarForm = document.querySelector('#header__form');
-// console.log(searchBarForm);
+
 // Получаем доступ к инпут
 const inputElementForm = document.querySelector('.header__input');
-// console.log(inputElementForm);
 
+// Место для вывода выпадающего списка при поиске в input, событие input
 const moviesSearchPlace = document.querySelector('.js-list-place');
-// console.log(moviesSearchPlace);
+
 // Функция с дебаунс
-// inputElementForm.addEventListener('input', debounce(moviesSearch, DEBOUNCE_DELAY));
+
 // Вешаем слушателя на инпут
-// inputElementForm.addEventListener('input', (event) => {
-// 	// Получаем значение введенное пользователем в input
-// 	const userInput = event.target.value;
 
-// 	// отслеживаем при введении значения
-// 	console.log(userInput);
-//   });
-
-// inputElementForm.addEventListener('input', moviesSearch);
 inputElementForm.addEventListener(
   'input',
   debounce(moviesSearch, DEBOUNCE_DELAY)
 );
 
-
-// moviesSearchPlace.addEventListener('click', event => {
-// 	// Проверяем есть наш li кликнули? closest ближ родит элемент.
-// 	// if (event.target.matches('.search__item-list' || ".search__item-descr" || ".search__item-name" || ".search__item-genres"))
-// 	if (event.target.closest('.search__item-list, .search__item-descr, .search__item-name, .search__item-genres'))  { 
-// 			const liElement = event.target.parentElement;
-// 			console.log("кликнули по li", liElement)
-// 			const elementId = liElement.dataset.id;
-	
-// 			console.log(elementId)
-// 		}
-	
-// 	})
-
-// Получаем id фильма при клике на элемент списка возле input
-
-moviesSearchPlace.addEventListener('click', event => {
-    const liElement = event.target.closest('.search__item-list');
-    if (liElement) { 
-        console.log("Кликнули по li", liElement);
-        const elementId = liElement.dataset.id;
-        console.log(" ID фильма ",elementId);
-// Добавить фокус на выбранный элемент li
-liElement.focus();
-
-
-
-    } else {
-        // Если кликнули на другом элементе, игнорируем его
-        return;
-    }
-});
-
-
-
-// const newListElements = document.querySelectorAll('.new-item')
-// console.log(newListElements)
-
 function moviesSearch (event) {
-	event.preventDefault();
+  event.preventDefault();
   // отслеживаем при введении значения
   const userInputSearchData = event.target.value.trim();
-
-  // console.log(userInputSearchData);
 
   // получаем с сервера запрос по импуту
   getMoviesByName(userInputSearchData, pageState).then(({ data }) => {
     // записываем результат данных с сервера в переменную
     const arrayOfResultsSearch = data.results;
-    // console.log(arrayOfResultsSearch);
-	if(userInputSearchData){
-		messegeSearchFailed.style.display = 'none';
-	}
+
+    if (userInputSearchData) {
+      messegeSearchFailed.style.display = 'none';
+    }
     moviesListResultsMarkup(arrayOfResultsSearch);
   });
 }
 
-function moviesListResultsMarkup(movies, index) {
+function moviesListResultsMarkup (movies, data) {
   let counter = 0; // Инициализация счетчика
-	const searchItem = movies
-	  .map(element => {
+  const searchItem = movies
+    .map(element => {
       // При прохождении счетчик добавляет 1 и if (counter <= 7) остановится. Можно менять сколько надо результатов выводить
-      counter++
-		// console.log(getGenres(element.genre_ids[0]))
-    if (counter <= 7) {
-      return `
-        <li class="search__item-list" data-id="${element.id}">
+      counter++;
+
+      if (counter <= 7) {
+        // if(id !== element.id)
+        return `
+        <li class="search__item-list" data-release="${element.release_date.slice(
+          0,
+          4
+        )}", data-query="${element.title || element.name} ">
           <p class="search__item-descr">
             <span class="search__item-name">
               ${element.title || element.name}
             </span>
             <span class="search__item-genres">
-              (${element.release_date.slice(0, 4)})
+              ${element.release_date.slice(0, 4)}
             </span>
           </p>
-        </li>
-      `;
-    }
-  }).join('');
+        </li>`;
+      }
+    })
+    .join('');
   moviesSearchPlace.innerHTML = searchItem;
 }
 
 // Функция Убираем список при нажатии ESC
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   // Проверяем, является ли нажатая клавиша клавишей Esc
   if (event.key === 'Escape') {
-    // Удаляем 
+    // Удаляем меню поиска, при нажатии ESC
     moviesSearchPlace.innerHTML = '';
   }
 });
@@ -146,52 +106,77 @@ document.addEventListener('keydown', function(event) {
 // Функция Убирает список поиска под инпутом при нажатии в любом месте. Нажать в любом месте кроме обьекта списка
 
 // Добавляем обработчик событий на весь документ
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function (event) {
   // Проверяем, является ли элемент клика ребёнком moviesSearchPlace или самим moviesSearchPlace
-  if (!moviesSearchPlace.contains(event.target) && event.target !== moviesSearchPlace) {
+  if (
+    !moviesSearchPlace.contains(event.target) &&
+    event.target !== moviesSearchPlace
+  ) {
     // Удаляем или скрываем список в этом случае
     moviesSearchPlace.innerHTML = '';
   }
 });
 
-// Тут функция рендера на страницу, результата извыпадающего списка, на который кликнул пользователь
+// функция рендера на страницу, результата извыпадающего списка, на который кликнул пользователь
 
+// Получаем id фильма при клике на элемент списка возле input
 
+moviesSearchPlace.addEventListener('click', event => {
+  const liElement = event.target.closest('.search__item-list');
+  if (liElement) {
+    // console.log("Кликнули по li", liElement);
+    // Тут название фильма
+    const elementName = liElement.dataset.query;
+    const elementData = liElement.dataset.release;
 
+    // console.log(" Name фильма ",elementName);
+    // Добавить фокус на выбранный элемент li
+    liElement.focus();
 
+    // Тут вставляем функцию поиска по ID или маркап
 
-// function moviesListResultsMarkup (movies) {
-//   const searchItem = movies
-//     .map(element => {
-//       // console.log(getGenres(element.genre_ids[0]))
-//       return `
-// 	  <li class="search__item-list" data-id="${element.id}">
-// 	<p class="search__item-descr" >
-// 	  <span class="search__item-name">
-// 		${element.title || element.name}
-// 	  </span>
-// 	  <span class="search__item-genres">
-// 	  (${(element.release_date.slice(0, 4))})
-// 	     </span>
-// 	</p>
-//   </li>
-// 	`;
-//     })
-//     .join('');
-//   moviesSearchPlace.innerHTML = searchItem;
-// }
+    getMovieByNameRelease(elementName, pageState, elementData).then(
+      ({ data }) => {
+        // записываем результат данных с сервера в переменную
+        const arrayOfResults = data.results;
+        // console.log(arrayOfResults)
+
+        // Тут условие проверки  надо вставить или фильт
+
+        //  const filtrName = arrayOfResults.filter(item=> item.name == elementName)
+        // console.log(" Data фильма ",elementData);
+        const filtrDate = arrayOfResults.filter(item =>
+          item.release_date.includes(elementData)
+        );
+        // console.log(item.release_date.includes(elementData))
+        // console.log(item.release_date)
+
+        //  console.log(" filter ",filtrDate);
+        //const newFilter = filtrDate.filter(item => item.title === elementName)
+
+        // console.log(filtrDate)
+        // if(elementName === ${element.title || element.name} & elementData === ${element.release_date.slice(0, 4)})
+        // if(`${element.title || element.name}` === elementName && `${element.release_date.slice(0, 4)}` === elementData)
+
+        renderList(document.querySelector('.gallery__list'), filtrDate);
+      }
+    );
+  } else {
+    // Если кликнули на другом элементе, игнорируем его
+    return;
+  }
+});
+
+// =======================================================================================================
 
 // ========================================================================================
-
-
-
+// Ниже функция действия searchBar по submit
 
 searchBarForm.addEventListener('submit', onSearchBarFormSubmit);
 
-async function onSearchBarFormSubmit(event) {
+async function onSearchBarFormSubmit (event) {
   event.preventDefault();
 
-  //   const onInput = formElement.query.value;
   // Значение поля input
   const onSearchBarInput = event.target.elements.query.value;
 
@@ -199,18 +184,17 @@ async function onSearchBarFormSubmit(event) {
   if (!onSearchBarInput) {
     Notiflix.Notify.failure('Please enter a keyword to continue the search');
     // тут можно дополнительно вызвывать функцию sendMessage () с каким то сообщением"поле поиска не заполнено"
-    // sendMessage ()
+
     event.preventDefault();
     return;
   }
 
-  // console.log(onSearchBarInput);
   // Обьект получаем
 
   getMoviesByName(onSearchBarInput, pageState).then(({ data }) => {
     // записываем результат данных с сервера в переменную
     const arrayOfResults = data.results;
-    // console.log(arrayOfResults)
+
     // Если результат не найден по ключевым словам выдаём сообщение ошибку
     if (!arrayOfResults.length > 0) {
       Notiflix.Notify.failure(
@@ -223,8 +207,10 @@ async function onSearchBarFormSubmit(event) {
       return;
     }
 
-    renderList(document.querySelector('.gallery__list'), data.results);
+    renderList(document.querySelector('.gallery__list'), arrayOfResults);
 
+    // ==================================================================================
+    // Пагинации функция находится ниже
     const galleryList = document.querySelector('.gallery__list');
 
     const pagination = createPagination(data.total_results, data.total_pages);
